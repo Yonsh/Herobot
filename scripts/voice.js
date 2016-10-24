6b9cb40aaf73c8602ed1739d6167e0a2f5c3d00b
Yonsh Lin <yonsh@live.com>
2016-10-24 22:35:34 +0800

    Add Google translate voice in Chinese

---
+++ scripts/voice.js
@@ -0,0 +1,28 @@
+// Description:
+//   Get google translate voice in Chinese
+
+module.exports = function(robot) {
+
+    robot.respond(/voice /i, function(res){
+      var msg = res.message.text;
+      msg = msg.substring(msg.indexOf("voice ") + 6);
+
+      if (msg !== '') {
+        robot.emit(
+          'telegram:invoke',
+          'sendAudio', {
+            chat_id: res.message.room,
+            audio: "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=zh-TW&q=" + msg.replace(" ", "%20"),
+            performer: "估狗小姐",
+            title: msg,
+            caption: msg
+          }, function (error, response) {
+            if (error) {
+              robot.logger.error(error);
+            }
+            robot.logger.debug(response);
+        });
+      }
+
+    });
+}
