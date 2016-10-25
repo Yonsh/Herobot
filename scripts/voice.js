685b0be67d87115b6c566d8b82544e17a65d241b
Yonsh Lin <yonsh@live.com>
2016-10-25 09:06:18 +0800

    Fix space issue

--- scripts/voice.js
+++ scripts/voice.js
@@ -12,7 +12,7 @@ module.exports = function(robot) {
           'telegram:invoke',
           'sendAudio', {
             chat_id: res.message.room,
-            audio: "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=zh-TW&q=" + msg.replace(" ", "%20"),
+            audio: "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=zh-TW&q=" + msg.replace(/[ \n]/g, "%20"),
             performer: "估狗小姐",
             title: msg,
             caption: msg
